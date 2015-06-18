5b641b1c41c5e935053c2c9fd8e86beb64559bcc
frost <simple1115@gmail.com>
2015-06-18 13:36:15 +0800

    task 1 accepted

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -4,15 +4,15 @@ public:
         int i, sum;  
         vector<int> results;  
         map<int, int> hmap;  
-        for (i=0; i<numbers.size(); i++){  
+        for (i = 0; i < numbers.size(); i++){  
             if(!hmap.count(numbers[i])){  
                 hmap.insert(pair<int, int>(numbers[i], i));  
             }  
             if (hmap.count(target - numbers[i])){  
-                int n=hmap[target - numbers[i]];  
-                if(n<i){  
-                    results.push_back(n+1);  
-                    results.push_back(i+1);  
+                int n = hmap[target - numbers[i]];  
+                if (n < i){  
+                    results.push_back(n + 1);  
+                    results.push_back(i + 1);  
                     return results;  
                 }  
             }  
