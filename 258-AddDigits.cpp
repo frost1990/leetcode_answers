cec759ca2dccf665f27efda977227b4ac4dbd34a
frost <simple1115@gmail.com>
2015-08-17 21:33:36 +0800

    task258 accepted

---
+++ 258-AddDigits.cpp
@@ -0,0 +1,32 @@
+/*
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
+#include <stdio.h>
+
+class Solution {
+public:
+	int addDigits(int num) {
+		return (num - 9 *((num - 1)/9));
+	}
+};
+
+int main() {
+
+	Solution s;
+
+	int ret = s.addDigits(12345);
+	
+	printf("Return value is %d\n", ret);
+
+	return 0;
+}
