d7b0a04c5521125998fe824be1705adb7d86b725
frost <simple1115@gmail.com>
2015-08-18 15:25:09 +0800

    task104 accepted

--- 104-MaximumDepthOfBinaryTree.cpp
+++ 104-MaximumDepthOfBinaryTree.cpp
@@ -5,17 +5,66 @@ The maximum depth is the number of nodes along the longest path from the root no
 */
 
 /**
- *  * Definition for a binary tree node.
- *   * struct TreeNode {
- *    *     int val;
- *     *     TreeNode *left;
- *      *     TreeNode *right;
- *       *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- *        * };
- *         */
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include <stdio.h>
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        
-    }
+	int maxDepth(TreeNode* root) {
+		if (root == NULL) {
+			return 0;
+		}
+		int lh = maxDepth(root->left);
+		int rh = maxDepth(root->right);
+
+		if (lh >= rh) {
+			return lh + 1;
+		} else {
+			return rh + 1;
+		}
+	}
 };
+
+/*
+			4
+		2		5
+	1		3		9
+				7
+*/
+
+int main() {
+	TreeNode t1(1), t2(2), t3(3), t4(4), t5(5), t6(6), t7(7), t8(8), t9(9);
+	t4.left = &t2;
+	t4.right = &t5;
+
+	t2.left = &t1;
+	t2.right = &t3;
+
+	t5.right = &t9;
+
+	t9.left = &t7;
+	t7.left = &t6;
+	t7.right = &t8;
+	
+	Solution s;
+	int ret = s.maxDepth(&t4);
+	
+	printf("Return value is %d\n", ret);
+
+	return 0;
+}
