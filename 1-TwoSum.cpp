a52fb2363f78f6a71f2b4fbb1a75a63e7ccca8fc
frost <simple1115@gmail.com>
2015-08-21 02:10:56 +0800

    rewrite task 1

--- 1-TwoSum.cpp
+++ 1-TwoSum.cpp
@@ -9,18 +9,24 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2 
 */
 
+#include <stdio.h>
+#include <vector>
+#include <map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         int i, sum;  
         vector<int> results;  
-        map<int, int> hmap;  
+        map<int, int> store;  
         for (i = 0; i < numbers.size(); i++){  
-            if(!hmap.count(numbers[i])){  
-                hmap.insert(pair<int, int>(numbers[i], i));  
+            if(!store.count(numbers[i])){  
+                store.insert(pair<int, int>(numbers[i], i));  
             }  
-            if (hmap.count(target - numbers[i])){  
-                int n = hmap[target - numbers[i]];  
+            if (store.count(target - numbers[i])){  
+                int n = store[target - numbers[i]];  
                 if (n < i){  
                     results.push_back(n + 1);  
                     results.push_back(i + 1);  
@@ -31,3 +37,20 @@ public:
         return results;  
     }  
 };
+
+int main() {
+	int array[5] = {2, 3, 4, 5, 6};
+	vector<int> source;
+	for (int i = 1; i <= 5; i++) {
+		source.push_back(array[i]);
+	}
+
+	Solution s;
+
+	vector<int> result = s.twoSum(source, 8);
+	for (size_t i = 0; i < result.size(); i++) {
+		printf("%d ", result[i]);
+	}
+	printf("\n");
+	return 0;
+}
